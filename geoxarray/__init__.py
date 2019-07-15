dec23ff05f377665150d406a6e66dd90342888ae
David Hoese <david.hoese@ssec.wisc.edu>
2019-07-15 13:54:20 -0500

    Switch to pyproj CRS object for all CRS operations
    And other cleanup

--- geoxarray/__init__.py
+++ geoxarray/__init__.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .crs import CRS
+from .crs import CRS  # noqa
 
 from ._version import get_versions
 __version__ = get_versions()['version']
--- geoxarray/crs/_proj4.py
+++ geoxarray/crs/_proj4.py
@@ -22,7 +22,7 @@ functions.
 
 """
 
-from pyproj import Proj, Geod
+from pyproj import CRS
 import logging
 
 LOG = logging.getLogger(__name__)
@@ -34,39 +34,17 @@ PROJ_TO_GX = {
 }
 
 
-def proj4_to_geoxarray(**kwargs):
-    """Convert a PROJ.4 dict to a dict of geoxarray CRS parameters."""
-    raise NotImplementedError()
-
-
-def geoxarray_to_proj4(**kwargs):
-    """Convert a dict of geoxarray CRS parameters to a PROJ.4 dict."""
-    raise NotImplementedError()
-
-
 def proj4_str_to_dict(proj4_str):
     """Convert PROJ.4 compatible string definition to dict
 
     Note: Key only parameters will be assigned a value of `True`.
     """
-    pairs = (x.split('=', 1) for x in proj4_str.replace('+', '').split(" "))
-    return dict((x[0], (x[1] if len(x) == 2 else True)) for x in pairs)
+    return CRS(proj4_str).to_dict()
 
 
 def proj4_dict_to_str(proj4_dict, sort=False):
     """Convert a dictionary of PROJ.4 parameters to a valid PROJ.4 string"""
-    items = proj4_dict.items()
-    if sort:
-        items = sorted(items)
-    params = []
-    for key, val in items:
-        key = str(key) if key.startswith('+') else '+' + str(key)
-        if key in ['+no_defs', '+no_off', '+no_rot']:
-            param = key
-        else:
-            param = '{}={}'.format(key, val)
-        params.append(param)
-    return ' '.join(params)
+    return CRS(proj4_dict).to_proj4()
 
 
 def proj4_radius_parameters(proj4_dict):
@@ -78,28 +56,5 @@ def proj4_radius_parameters(proj4_dict):
     Returns:
         a (float), b (float): equatorial and polar radius
     """
-    if isinstance(proj4_dict, str):
-        new_info = proj4_str_to_dict(proj4_dict)
-    else:
-        new_info = proj4_dict.copy()
-
-    # load information from PROJ.4 about the ellipsis if possible
-    if 'ellps' in new_info:
-        geod = Geod(**new_info)
-        new_info['a'] = geod.a
-        new_info['b'] = geod.b
-    elif 'a' not in new_info or 'b' not in new_info:
-
-        if 'rf' in new_info and 'f' not in new_info:
-            new_info['f'] = 1. / float(new_info['rf'])
-
-        if 'a' in new_info and 'f' in new_info:
-            new_info['b'] = float(new_info['a']) * (1 - float(new_info['f']))
-        elif 'b' in new_info and 'f' in new_info:
-            new_info['a'] = float(new_info['b']) / (1 - float(new_info['f']))
-        else:
-            geod = Geod(**{'ellps': 'WGS84'})
-            new_info['a'] = geod.a
-            new_info['b'] = geod.b
-
-    return float(new_info['a']), float(new_info['b'])
+    crs = CRS(proj4_dict)
+    return crs.semi_major_metre, crs.semi_minor_metre
